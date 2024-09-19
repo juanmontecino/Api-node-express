1f31209ac709691ebd7b64b790509262b39dc0ad
axelrust1 <nahuel.rustt@gmail.com>
2024-09-18 21:11:08 -0300

    cambio

--- routes/empleados.js
+++ routes/empleados.js
@@ -4,6 +4,6 @@ const { getEmpleados, getEmpleado } = require('../controllers/empleados')
 const rutas = Router()
 
 rutas.get('/', getEmpleados)
-rutas.get('/:idEmpleadoAswl', getEmpleado)
+rutas.get('/:idEmpleado', getEmpleado)
 
 module.exports = rutas
