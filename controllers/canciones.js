f063c2d5a44b739416f7477d8b08def3bc054dde
axelrust1 <nahuel.rustt@gmail.com>
2024-09-24 19:49:39 -0300

    agrego la ruta para mostrar cierta cantidad de canciones

--- controllers/canciones.js
+++ controllers/canciones.js
@@ -36,7 +36,48 @@ const getCanciones = (req = request, res = response) => {
     });
   }
 };
+const getCancion = (req = request, res = response) => {
+  const { cantidad = '' } = req.params; //recibimos la cantidad de canciones por parametro
+  const apiKey = process.env.APIKEYMUSICA;
+
+  if (!apiKey) {
+    return res.status(500).json({
+      msg: 'Error: API key no configurada',
+    });
+  }
+
+  try {
+    //muestra del genero rkt
+    const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=RKT&api_key=${apiKey}&format=json`;
+
+    axios.get(apiUrl)
+      .then((response) => {
+        const { tracks } = response.data;
+
+        //limita la cantidad de cancioens y las guarda en una variable para manejarlas mejor cuando las mostremos
+        const cancionesLimitadas = tracks.track.slice(0, parseInt(cantidad));
+
+        res.status(200).json({
+          msg: `Las ${cantidad} canciones más conocidas en el género pop`,
+          canciones: cancionesLimitadas,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400).json({
+          msg: 'Error al obtener las canciones',
+          error,
+        });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      msg: 'Error interno del servidor',
+    });
+  }
+}
 
 module.exports = {
-    getCanciones
+    getCanciones,
+    getCancion
    }
--- routes/canciones.js
+++ routes/canciones.js
@@ -1,7 +1,8 @@
 const { Router } = require ('express')
-const {getCanciones} = require('../controllers/canciones')
+const {getCanciones, getCancion} = require('../controllers/canciones')
 
 const rutas = Router()
 
 rutas.get('/', getCanciones) ///canciones?genero=pop ejemplo para buscar
+rutas.get('/:cantidad', getCancion) //canciones/10 para buscar 10 canciones del genero pop
 module.exports = rutas
